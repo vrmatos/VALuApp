b1d489a146ae2c0bb5acbecf1e71dab2f7a4bd31
vrmatos <vrmatos@loyola.edu>
2020-11-22 21:40:59 -0500

    Designs from Franceschi Meeting finished

--- app/src/main/java/com/victoriamatos/valuapp/MainActivity.java
+++ app/src/main/java/com/victoriamatos/valuapp/MainActivity.java
@@ -2,13 +2,20 @@ package com.victoriamatos.valuapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accessibilityservice.AccessibilityService;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.individual_request_screen);
+        setContentView(R.layout.browse_requests_screen);
+
+        LayoutInflater inflater = (LayoutInflater)this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
     }
 }
